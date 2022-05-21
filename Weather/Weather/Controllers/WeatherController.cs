e522fb448734147177c28626b6afdf12879212f9
Andriy_Sorochuk <sag0702@gmail.com>
2022-05-21 10:21:09 +0200

    Added initial code

---
+++ Weather/Weather/Controllers/WeatherController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Weather.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherController : ControllerBase
+    {
+
+        private readonly ILogger<WeatherController> _logger;
+
+        public WeatherController(ILogger<WeatherController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return null;
+        }
+    }
+}
---
+++ Weather/Weather/WeatherForecast.cs
@@ -0,0 +1,11 @@
+namespace Weather
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int Temperature { get; set; }
+
+        public string? Summary { get; set; }
+    }
+}
